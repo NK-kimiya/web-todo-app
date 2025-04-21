66be1ff5bd5b0a3865b1fed4345fa76ace590c03
root <root@DESKTOP-EMLN77C>
2025-04-22 02:13:45 +0900

    TODOアプリ完成

--- app/routes/web.php
+++ app/routes/web.php
@@ -16,3 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// ここから追加
+
+//TodoController に「RESTfulな7つの基本ルート」を自動で設定する
+Route::resource('todo', 'App\Http\Controllers\TodoController');
+
+//URLが /todo/done/{todo} に PUTリクエストされたら、
+//TodoController::done() を実行
+//{todo} はルートパラメータ（例えば /todo/done/3 なら 3 を受け取る
+Route::put('todo/done/{todo}', [App\Http\Controllers\TodoController::class, 'done'])->name('todo.done');
+//URLが /todo/undone/{todo} にPUTされたら、
+//TodoController::undone() を実行する
+//ルートの名前は todo.undone
+Route::put('todo/undone/{todo}', [App\Http\Controllers\TodoController::class, 'undone'])->name('todo.undone');
+//　ここまで追加
